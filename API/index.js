bbfd656dd889473c2492668127a601652c9e7f09
ZayaniFarah <zayanifarah93@gmail.com>
2025-05-20 09:57:02 +0100

    Remove unused Node.js files and update .gitignore; clean up requirements.txt formatting.

--- API/index.js
+++
@@ -1,6 +0,0 @@
-// This file is not used in the Django project
-// The main entry point is manage.py
-
-// run `node index.js` in the terminal
-
-console.log(`Hello Node.js v${process.versions.node}!`);
